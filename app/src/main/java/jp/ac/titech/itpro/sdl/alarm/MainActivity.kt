0c25c28b107e42e8c6e637db475fb134110167c8
mds_boy <modasokun@gmail.com>
2021-07-03 16:35:23 +0900

    initial commit

---
+++ app/src/main/java/jp/ac/titech/itpro/sdl/alarm/MainActivity.kt
@@ -0,0 +1,16 @@
+package jp.ac.titech.itpro.sdl.alarm
+
+import android.app.TimePickerDialog
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TimePicker
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
