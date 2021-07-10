c0780dffd21165e80308e4838a525d8b47787d79
mds_boy <modasokun@gmail.com>
2021-07-11 01:02:02 +0900

    振動を追加

--- app/src/main/java/jp/ac/titech/itpro/sdl/alarm/AlarmReceiver.kt
+++ app/src/main/java/jp/ac/titech/itpro/sdl/alarm/AlarmReceiver.kt
@@ -7,7 +7,12 @@ import android.media.AudioFormat
 import android.media.AudioRecord
 import android.media.MediaPlayer
 import android.media.MediaRecorder
+import android.os.Build
+import android.os.VibrationEffect
+import android.os.VibrationEffect.DEFAULT_AMPLITUDE
+import android.os.Vibrator
 import android.util.Log
+import androidx.annotation.RequiresApi
 import kotlin.math.abs
 
 
@@ -25,6 +30,8 @@ class AlarmReceiver : BroadcastReceiver() {
     val audioDataArray = ShortArray(section)
 
     private var mediaPlayer: MediaPlayer? = null
+    private var vibrator: Vibrator? = null
+
     private var audioRecord: AudioRecord = AudioRecord(
         MediaRecorder.AudioSource.MIC,
         samplingRate,
@@ -33,15 +40,27 @@ class AlarmReceiver : BroadcastReceiver() {
         bufSize
     )
 
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onReceive(context: Context?, intent: Intent?) {
 
         Log.d("debug", "received")
 
-        if (mediaPlayer == null) {
+        if (mediaPlayer == null && vibrator == null) {
             mediaPlayer = MediaPlayer.create(context, R.raw.sound_file_1)
+            vibrator = context?.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        } else {
+            return
         }
 
-        mediaPlayer!!.start()
+        mediaPlayer?.start()
+
+        val vibrationEffect = VibrationEffect.createWaveform(
+            longArrayOf(300, 300),
+            intArrayOf(0, DEFAULT_AMPLITUDE),
+            0
+        )
+        vibrator?.vibrate(vibrationEffect)
 
         audioRecord.positionNotificationPeriod = section
 
@@ -50,21 +69,22 @@ class AlarmReceiver : BroadcastReceiver() {
         audioRecord.setRecordPositionUpdateListener(object :
             AudioRecord.OnRecordPositionUpdateListener {
             override fun onPeriodicNotification(recorder: AudioRecord) {
-                recorder.read(audioDataArray, 0, section) // 音声データ読込
+                recorder.read(audioDataArray, 0, section)
                 Log.v("AudioRecord", "onPeriodicNotification size=${audioDataArray.size}")
 
                 val sumLevel = audioDataArray.map { abs(it.toDouble()) }.sum()
                 val amplitude = sumLevel / section
 
                 Log.v("AudioRecord", "amplitude=${amplitude}")
-                if (amplitude > 1000)
-                    mediaPlayer!!.stop()
+                if (amplitude > 1000) {
+                    mediaPlayer?.stop()
+                    vibrator?.cancel()
+                }
             }
 
             override fun onMarkerReached(recorder: AudioRecord) {
-                recorder.read(audioDataArray, 0, section) // 音声データ読込
+                recorder.read(audioDataArray, 0, section)
                 Log.v("AudioRecord", "onMarkerReached size=${audioDataArray.size}")
-                // 好きに処理する
             }
         })
 
